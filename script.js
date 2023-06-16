8fb5cd996d2c9907e6098abb9aeb05159f7a5e77
Maxim4401 <129541316+Maxim4401@users.noreply.github.com>
2023-06-16 11:57:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function A() {
+    console.log("Hide Activated")
+    alert("Please can you get of this page it is private")
+    document.getElementById('h1_id').textContent = 'Go now';
+    document.getElementById('h2_id').textContent = 'Go now';
+    document.getElementById('b1').textContent = 'Go now';
+}
